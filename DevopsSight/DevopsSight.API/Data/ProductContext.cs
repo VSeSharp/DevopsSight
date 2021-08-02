7207fdb9312d0d092947ebce5ff24e65d79e169a
VSeSharp <vitaliy.senchak@gmail.com>
2021-08-02 13:08:07 +0200

    DB change

--- DevopsSight/DevopsSight.API/Data/ProductContext.cs
+++ DevopsSight/DevopsSight.API/Data/ProductContext.cs
@@ -35,7 +35,7 @@ namespace DevopsSight.API.Data
                 {
                     Name = "IPhone X",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-1.png",
+                    ImageFile = "VSeSharp.png",
                     Price = 950.00M,
                     Category = "Smart Phone"
                 },
@@ -43,7 +43,7 @@ namespace DevopsSight.API.Data
                 {
                     Name = "Samsung 10",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-2.png",
+                    ImageFile = "VSeSharp.png",
                     Price = 840.00M,
                     Category = "Smart Phone"
                 },
@@ -51,7 +51,7 @@ namespace DevopsSight.API.Data
                 {
                     Name = "Huawei Plus",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-3.png",
+                    ImageFile = "VSeSharp.png",
                     Price = 650.00M,
                     Category = "White Appliances"
                 },
@@ -59,7 +59,7 @@ namespace DevopsSight.API.Data
                 {
                     Name = "Xiaomi Mi 9",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-4.png",
+                    ImageFile = "VSeSharp.png",
                     Price = 470.00M,
                     Category = "White Appliances"
                 },
@@ -67,7 +67,7 @@ namespace DevopsSight.API.Data
                 {
                     Name = "HTC U11+ Plus",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-5.png",
+                    ImageFile = "VSeSharp.png",
                     Price = 380.00M,
                     Category = "Smart Phone"
                 },
@@ -75,7 +75,7 @@ namespace DevopsSight.API.Data
                 {
                     Name = "LG G7 ThinQ EndofCourse",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    ImageFile = "product-6.png",
+                    ImageFile = "VSeSharp.png",
                     Price = 240.00M,
                     Category = "Home Kitchen"
                 }
